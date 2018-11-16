287ce7a4bf66a7f17b132b7578a07109cc048152
amined111 <43893689+amined111@users.noreply.github.com>
2018-11-16 01:10:26 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,15 +7,30 @@ client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id !== "426033902319304705") return;
+  if (message.author.id !== "485917652120764429") return;
  
 if (message.content.startsWith(prefix + 'setstream')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/hafsa121");
+  client.user.setGame(argresult, "https://www.twitch.tv/amine12a");
      console.log('test' + argresult);
-    message.channel.sendMessage(`Streaming: **${argresult}`)
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
 }
+if (message.content.startsWith(prefix + 'setgame')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} 
 
+if (message.content.startsWith(prefix + 'setwatch')) {
+client.user.setActivity(argresult, {type:'WATCHING'});
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} 
+if (message.content.startsWith(prefix + 'setlisteng')) {
+client.user.setActivity(argresult, {type:'LISTENING'});
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+}
+  
 });
+
+
 
 
 client.login(process.env.BOT_TOKEN);
