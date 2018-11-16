79ba3f07614130efedcfb1a7a4c76fdc2775f858
amined111 <43893689+amined111@users.noreply.github.com>
2018-11-16 01:23:50 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id !== "485917652120764429") return;
+  if (message.author.id !== "426033902319304705") return;
  
 if (message.content.startsWith(prefix + 'setstream')) {
   client.user.setGame(argresult, "https://www.twitch.tv/amine12a");
