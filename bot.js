3f09a0cb4561711f7b6f1c27fb7c16bcd805c481
amined111 <43893689+amined111@users.noreply.github.com>
2018-11-18 18:49:51 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Aesthetic without any manufacture`,"http://twitch.tv/S-F")
+client.user.setGame(`Tnakt`,"http://twitch.tv/S-F")
 client.user.setStatus("dnd")
 });
 
