2c67692c55caf15b7cbe148d15ccb4cdc03d1e27
amined111 <43893689+amined111@users.noreply.github.com>
2018-11-20 22:35:37 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,12 +2,12 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on('message', msg => {
-var prefix = "!";//البرفكس
+var prefix = "H";//البرفكس
   var args = msg.content.split(' ').slice(1);
   var argresult = args.join(' ');
 if (msg.author.bot) return;
 if(msg.content.startsWith(prefix + "setstream")) {
-      if(msg.author.id != "426033902319304705") return msg.reply("هذا الامر لصحاب الحساب");
+      if(msg.author.id != "485917652120764429") return msg.reply("هذا الامر لصحاب الحساب");
       client.user.setGame(${argresult},"http://twitch.tv/S-F%22")
       var Die = new Discord.RichEmbed()
       .setTitle(":white_check_mark:تم تغير الستريمنق الخاص بك")
@@ -22,4 +22,3 @@ if(msg.content.startsWith(prefix + "setstream")) {
 
 
 client.login(process.env.BOT_TOKEN);
-client.login(process.env.BOT_TOKEN1);
